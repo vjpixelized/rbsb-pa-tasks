4b0bb6b2e86a6eeebad7bc632f03840bbeb6764d
vjpixelized <vjpixelized@gmail.com>
2026-06-18 13:46:12 -0700

    Connect Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.PA_TASKS_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://avghkmpnctggziwfssqk.supabase.co",
+  anonKey: "sb_publishable_rk-f7dnWr0K4XY1G19C7GQ_YCrrb5Uq",
   table: "pa_tasks"
 };
